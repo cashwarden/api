aa823632a4c80bc7926e6635900b173911c02bdd
forecho <caizhenghai@gmail.com>
2020-08-06 22:50:15 +0800

    fix: telegram hook tests

--- modules/v1/controllers/TelegramController.php
+++ modules/v1/controllers/TelegramController.php
@@ -4,7 +4,7 @@ namespace app\modules\v1\controllers;
 
 use app\core\traits\ServiceTrait;
 use TelegramBot\Api\BotApi;
-use TelegramBot\Api\Types\Message;
+use TelegramBot\Api\Types\Update;
 
 class TelegramController extends ActiveController
 {
@@ -29,9 +29,20 @@ class TelegramController extends ActiveController
         try {
             # $bot = TelegramService::newClient();
             $bot = new \TelegramBot\Api\Client(params('telegramToken'));
-            $bot->command('ping', function (Message $message) use ($bot) {
+//            $bot->command('ping', function (Message $message) use ($bot) {
+//                /** @var BotApi $bot */
+//                $bot->sendMessage($message->getChat()->getId(), 'pong!');
+//            });
+
+            $bot->on(function (Update $Update) use ($bot) {
+                $message = $Update->getMessage();
                 /** @var BotApi $bot */
-                $bot->sendMessage($message->getChat()->getId(), 'pong!');
+                $bot->sendMessage($message->getChat()->getId(), "hi");
+            }, function (Update $message) use ($bot) {
+                if ($message->getMessage() == '/login') {
+                    return true;
+                }
+                return false;
             });
 
             $bot->run();
