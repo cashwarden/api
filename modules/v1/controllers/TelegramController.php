eb027b3e0d7f41572d87685b73d95a351bf4b7eb
forecho <caizhenghai@gmail.com>
2020-08-06 23:08:37 +0800

    fix: telegram hook tests

--- modules/v1/controllers/TelegramController.php
+++ modules/v1/controllers/TelegramController.php
@@ -27,6 +27,26 @@ class TelegramController extends ActiveController
      */
     public function actionHook()
     {
+        $path = "https://api.telegram.org/bot" . params('telegramToken');
+
+        $update = json_decode(file_get_contents("php://input"), true);
+
+        $chatId = $update["message"]["chat"]["id"];
+        $message = $update["message"]["text"];
+
+        if (strpos($message, "/weather") === 0) {
+            $location = substr($message, 9);
+            $weather = json_decode(
+                file_get_contents("http://api.openweathermap.org/data/2.5/weather?q={$location}&appid=mytoken"),
+                true
+            )["weather"][0]["main"];
+            file_get_contents(
+                "{$path}/sendmessage?chat_id={$chatId}&text=Here's the weather in {$location}:{$weather}"
+            );
+        }
+        die;
+
+
         try {
             # $bot = TelegramService::newClient();
             $bot = new \TelegramBot\Api\Client(params('telegramToken'));
