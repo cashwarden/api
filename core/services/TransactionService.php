2f6b76bfcdd946f485afb0c11bc73d4a6e009216
forecho <caizhenghai@gmail.com>
2020-09-03 21:48:06 +0800

    fix: fix formatRecords

--- core/services/TransactionService.php
+++ core/services/TransactionService.php
@@ -159,7 +159,7 @@ class TransactionService extends BaseObject
     {
         $items = [];
         foreach ($records as $record) {
-            $key = Yii::$app->formatter->asDatetime(strtotime($record->date), 'php:Ymd');
+            $key = Yii::$app->formatter->asDatetime(strtotime($record->date), 'php:Y-m-d');
             $items[$key]['records'][] = $record;
             $items[$key]['date'] = $key;
             if ($record->transaction_id) {
