d88d2e35a74ecf6a8ea94ec037e8fc1f79a0509c
forecho <caizhenghai@gmail.com>
2020-09-17 11:45:40 +0800

    feat: Logs do not record sensitive information

--- config/params.php
+++ config/params.php
@@ -10,4 +10,9 @@ return [
     'seoKeywords' => env('SEO_KEYWORDS'),
     'seoDescription' => env('SEO_DESCRIPTION'),
     'googleAnalyticsAU' => env('GOOGLE_ANALYTICS_AU'),
+    // 不记录 header 指定 key 的值到日志，默认值为 authorization，配置自定义会覆盖默认值
+    'logFilterIgnoredHeaderKeys' => env('LOG_FILTER_IGNORED_HEADER_KEYS', 'authorization,token,cookie'),
+    'logFilterIgnoredKeys' => env('LOG_FILTER_IGNORED_KEYS', 'password'), // 不记录日志
+    'logFilterHideKeys' => env('LOG_FILTER_HIDE_KEYS'), // 用*代替所有数据
+    'logFilterHalfHideKeys' => env('LOG_FILTER_HALF_HIDE_KEYS', 'email'), // 部分数据隐藏，只显示头部 20% 和尾部 20% 数据，剩下的用*代替
 ];
--- core/behaviors/LoggerBehavior.php
+++ core/behaviors/LoggerBehavior.php
@@ -28,13 +28,23 @@ class LoggerBehavior extends Behavior
         $response = $event->sender;
         if ($response->format != 'html') {
             $request = \Yii::$app->request;
+            $params = Yii::$app->params;
             $requestId = Yii::$app->requestId->id;
             $code = ArrayHelper::getValue($response->data, 'code');
+
+            $ignoredKeys = explode(',', ArrayHelper::getValue($params, 'logFilterIgnoredKeys', ''));
+            $hideKeys = explode(',', ArrayHelper::getValue($params, 'logFilterHideKeys', ''));
+            $halfHideKeys = explode(',', ArrayHelper::getValue($params, 'logFilterHalfHideKeys', ''));
+            $ignoredHeaderKeys = explode(',', ArrayHelper::getValue($params, 'logFilterIgnoredHeaderKeys', ''));
+            $requestHeaderParams = $this->headerFilter($request->headers->toArray(), $ignoredHeaderKeys);
+
+            $requestParams = $this->paramsFilter($request->bodyParams, $ignoredKeys, $hideKeys, $halfHideKeys);
+
             $message = [
                 'request_id' => $requestId,
                 'type' => $code === 0 ? 'response_data_success' : 'response_data_error',
-                'header' => Json::encode($request->headers),
-                'params' => $request->bodyParams,
+                'header' => Json::encode($requestHeaderParams),
+                'params' => Json::encode($requestParams),
                 'url' => $request->absoluteUrl,
                 'response' => Json::encode($response->data)
             ];
@@ -48,5 +58,81 @@ class LoggerBehavior extends Behavior
     public function beforeAction()
     {
         return Yii::$app->requestId->id;
+    }
+
+
+    /**
+     * @param array $params
+     * @param array $ignoredHeaderKeys
+     * @return array
+     */
+    protected function headerFilter(array $params, array $ignoredHeaderKeys)
+    {
+        foreach ($params as $key => $item) {
+            if ($key && in_array($key, $ignoredHeaderKeys)) {
+                unset($params[$key]);
+            }
+        }
+        return $params;
+    }
+
+    /**
+     * @param $params array
+     * @param array $ignoredKeys
+     * @param array $hideKeys
+     * @param array $halfHideKeys
+     * @return string|int|array
+     */
+    protected function paramsFilter(array $params, array $ignoredKeys, array $hideKeys, array $halfHideKeys)
+    {
+        if (!$hideKeys && !$halfHideKeys && !$ignoredKeys) {
+            return $params;
+        }
+        foreach ($params as $key => &$item) {
+            if (is_array($item)) {
+                $item = $this->paramsFilter($item, $ignoredKeys, $hideKeys, $halfHideKeys);
+                continue;
+            }
+            if ($key && in_array($key, $ignoredKeys)) {
+                unset($params[$key]);
+            } elseif ($key && in_array($key, $hideKeys)) {
+                $item = $this->paramReplace($key, $item);
+            } elseif ($key && in_array($key, $halfHideKeys)) {
+                $item = $this->paramPartialHiddenReplace($key, $item);
+            }
+        }
+        return $params;
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     * @return string
+     */
+    protected function paramReplace(string $key, $value)
+    {
+        return str_repeat('*', strlen($value));
+    }
+
+
+    /**
+     * @param string $key
+     * @param $value
+     * @return string
+     */
+    protected function paramPartialHiddenReplace(string $key, $value)
+    {
+        $valueLength = strlen($value);
+        if ($valueLength > 2) {
+            $showLength = ceil($valueLength * 0.2);
+            $hideLength = $valueLength - $showLength * 2;
+            $newValue = mb_substr($value, 0, $showLength)
+                . str_repeat('*', $hideLength)
+                . mb_substr($value, -1 * $showLength);
+        } else {
+            $newValue = $this->paramReplace($key, $value);
+        }
+
+        return $newValue;
     }
 }
