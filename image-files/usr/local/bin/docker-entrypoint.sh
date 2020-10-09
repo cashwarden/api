a91198856b6ae2f39b4979c404dcb69768460f6c
forecho <caizhenghai@gmail.com>
2020-10-09 11:33:56 +0800

    feat: add image files

---
+++ image-files/usr/local/bin/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+printf "Checking database connection...\n\n"
+mysql_ready() {
+    mysqladmin ping --host=mysql --user=root --password=$MYSQL_ROOT_PASSWORD > /dev/null 2>&1
+}
+
+while !(mysql_ready)
+do
+    sleep 3
+    echo "Waiting for database connection ..."
+done
+
+printf "Upgrading database...\n\n"
+./yii migrate/up --interactive=0
+
+touch /srv/.migrated
+
+exec "$@"
